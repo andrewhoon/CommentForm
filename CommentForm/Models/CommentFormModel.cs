2e9ff135e60f8737f341b01c0937de900093adf3
andrewhoon <hoonandrew1709@gmail.com>
2017-02-07 14:41:59 -0500

    added controller for model

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace CommentForm.Models
 {
     public class CommentFormModel
     {
+        //our id keeps track of each instance
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
+
+
     }
 }
